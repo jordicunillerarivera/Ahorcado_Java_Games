dedf8735ffdd129556a0427f346bcf7ae5df7810
jordicunillerarivera <87013279+jordicunillerarivera@users.noreply.github.com>
2021-08-10 12:22:14 +0200

    Cambio nombre paquete

---
+++ java/com/AdriAhorcado/game/AdriAhorcado/App.java
@@ -0,0 +1,12 @@
+package com.AdriAhorcado.game.AdriAhorcado;
+
+import objetos.*;
+
+public class App {
+	@SuppressWarnings("unused")
+    public static void main( String[] args ) {
+
+		AplicacionGrafica aplicacionGrafica = new AplicacionGrafica();
+		
+    }
+}
